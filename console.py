9533400fcdb6de68a8890c72254d2a1b691241da
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-08-04 16:05:05 +0100

    arg validation

--- console.py
+++ console.py
@@ -20,6 +20,31 @@ class HBNBCommand(cmd.Cmd):
         "Quit command to exit the program"
         return True
 
+    def do_create(self, arg):
+        "Creates a new instance of class"
+        args = self.validate_arg(arg, False)
+    
+    def validate_arg(self, arg, check_for_id):
+        """
+        validate argument string
+        Args:
+            arg (string): argument string
+            check_for_id (bool): check if id is present in arg
+        """
+        classnames = ["BaseModel"]
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        elif args[0] not in classnames:
+            print("** class doesn't exist **")
+            return False
+        if check_for_id:
+            if len(args) < 2:
+                print("** instance id missing **")
+                return False
+            #check if instance
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
